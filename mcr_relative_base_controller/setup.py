5f4beec86a64a420877cbc267e70f9e9ce849fd6
Alexander Moriarty <alexander@dal.ca>
2015-11-30 16:33:40 +0100

    Update setup.py for latest catkin release
    see: https://github.com/ros/catkin/issues/756
    And see #91 https://mas.b-it-center.de/gitgate/mas-group/mas_industrial_robotics/issues/91

--- mcr_relative_base_controller/setup.py
+++ mcr_relative_base_controller/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['mcr_relative_base_controller_ros'],
-    package_dir={'mcr_relative_base_controller_ros': 'ros/src'}
+    package_dir={'mcr_relative_base_controller_ros': 'ros/src/mcr_relative_base_controller_ros'}
 )
 
 setup(**d)
