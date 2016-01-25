abea182b27262eebb469a9e2917981d89166a2a1
jsanch2s <jose.sanchez.loza@gmail.com>
2016-01-25 19:31:46 +0100

    [collision_velocity_filter] Fix to avoid getting stuck

--- mcr_collision_velocity_filter/ros/include/mcr_collision_velocity_filter/collision_velocity_filter_node.h
+++ mcr_collision_velocity_filter/ros/include/mcr_collision_velocity_filter/collision_velocity_filter_node.h
@@ -1,14 +1,20 @@
-/*
- * collision_velocity_filter_node.h
+/*********************************************************************
+ * Software License Agreement (GPLv3 License)
  *
- *  Created on: Apr 15, 2015
- *      Author: Frederik Hegger
+ *  Copyright (c) 2015, Hochschule Bonn-Rhein-Sieg.
+ *  All rights reserved.
+ *
+ *********************************************************************/
+/**
+ * Author: Frederik Hegger
  */
 
-#ifndef COLLISION_VELOCITY_FILTER_NODE_H_
-#define COLLISION_VELOCITY_FILTER_NODE_H_
+#ifndef MCR_COLLISION_VELOCITY_FILTER_COLLISION_VELOCITY_FILTER_NODE_H
+#define MCR_COLLISION_VELOCITY_FILTER_COLLISION_VELOCITY_FILTER_NODE_H
 
 #include <exception>
+#include <string>
+#include <vector>
 
 #include <ros/ros.h>
 #include <geometry_msgs/PolygonStamped.h>
@@ -38,8 +44,10 @@ public:
 
 private:
     void oneLaserScanCallback(const sensor_msgs::LaserScan::ConstPtr &scan);
-    void twoSynchronizedLaserscanCallback(const sensor_msgs::LaserScan::ConstPtr &scan_1, const sensor_msgs::LaserScan::ConstPtr &scan_2);
-    void threeSynchronizedLaserscanCallback(const sensor_msgs::LaserScan::ConstPtr &scan_1, const sensor_msgs::LaserScan::ConstPtr &scan_2, const sensor_msgs::LaserScan::ConstPtr &scan_3);
+    void twoSynchronizedLaserscanCallback(const sensor_msgs::LaserScan::ConstPtr &scan_1,
+        const sensor_msgs::LaserScan::ConstPtr &scan_2);
+    void threeSynchronizedLaserscanCallback(const sensor_msgs::LaserScan::ConstPtr &scan_1,
+        const sensor_msgs::LaserScan::ConstPtr &scan_2, const sensor_msgs::LaserScan::ConstPtr &scan_3);
 
     void accumulateLaserScansToPointCloud(const std::vector<sensor_msgs::LaserScan> &scans);
 
@@ -57,8 +65,10 @@ private:
     message_filters::Subscriber<sensor_msgs::LaserScan> sub_scan_1;
     message_filters::Subscriber<sensor_msgs::LaserScan> sub_scan_2;
     message_filters::Subscriber<sensor_msgs::LaserScan> sub_scan_3;
-    boost::shared_ptr<Synchronizer<ApproximateTime<sensor_msgs::LaserScan, sensor_msgs::LaserScan> > > two_synced_laser_scans_;
-    boost::shared_ptr<Synchronizer<ApproximateTime<sensor_msgs::LaserScan, sensor_msgs::LaserScan, sensor_msgs::LaserScan> > > three_synced_laser_scans_;
+    boost::shared_ptr<Synchronizer<ApproximateTime<sensor_msgs::LaserScan,
+        sensor_msgs::LaserScan> > > two_synced_laser_scans_;
+    boost::shared_ptr<Synchronizer<ApproximateTime<sensor_msgs::LaserScan, sensor_msgs::LaserScan,
+        sensor_msgs::LaserScan> > > three_synced_laser_scans_;
 
     ros::Subscriber sub_footprint_;
     ros::Subscriber sub_twist_;
@@ -71,6 +81,11 @@ private:
     ros::Publisher pub_real_footprint_;
     ros::Publisher pub_soft_footprint_;
     ros::Publisher pub_hard_footprint_;
+
+    ros::Publisher pub_front_right_footprint_;
+    ros::Publisher pub_front_left_footprint_;
+    ros::Publisher pub_rear_right_footprint_;
+    ros::Publisher pub_rear_left_footprint_;
 
     CollisionVelocityFilter *collision_velocity_filter_;
 
@@ -97,4 +112,4 @@ private:
     std_msgs::String event_out_;
 };
 
-#endif /* COLLISION_VELOCITY_FILTER_NODE_H_ */
+#endif  // MCR_COLLISION_VELOCITY_FILTER_COLLISION_VELOCITY_FILTER_NODE_H
--- mcr_collision_velocity_filter/ros/src/collision_velocity_filter_node.cpp
+++ mcr_collision_velocity_filter/ros/src/collision_velocity_filter_node.cpp
@@ -1,14 +1,19 @@
-/*
- * collision_velocity_filter_node.cpp
+/*********************************************************************
+ * Software License Agreement (GPLv3 License)
  *
- *  Created on: Apr 15, 2015
- *      Author: Frederik Hegger
+ *  Copyright (c) 2015, Hochschule Bonn-Rhein-Sieg.
+ *  All rights reserved.
+ *
+ *********************************************************************/
+/**
+ * Author: Frederik Hegger
  */
 
 #include <mcr_collision_velocity_filter/collision_velocity_filter_node.h>
 
 CollisionVelocityFilterNode::CollisionVelocityFilterNode() :
-    footprint_msg_received(false), laser_scans_as_pcl_cloud_received(false), desired_twist_msg_received_(false), debug_mode_(false)
+        footprint_msg_received(false), laser_scans_as_pcl_cloud_received(false), desired_twist_msg_received_(false),
+        debug_mode_(false)
 {
     std::string footprint_parameter_name = "footprint";
     std::string footprint_topic_name = "footprint";
@@ -16,12 +21,13 @@ CollisionVelocityFilterNode::CollisionVelocityFilterNode() :
 
     ros::NodeHandle nh("~");
 
-    target_frame_ = "/base_link";   //ToDo: read as a parameter
+    nh.param<std::string>("target_frame", target_frame_, "/base_link");
 
     zero_velocities_.linear.x = zero_velocities_.linear.y = zero_velocities_.linear.z = 0.0;
     zero_velocities_.angular.x = zero_velocities_.angular.y = zero_velocities_.angular.z = 0.0;
 
-    sub_footprint_ = nh.subscribe < geometry_msgs::PolygonStamped > (footprint_topic_name, 1, &CollisionVelocityFilterNode::footprintCallback, this);
+    sub_footprint_ = nh.subscribe < geometry_msgs::PolygonStamped > (footprint_topic_name,
+        1, &CollisionVelocityFilterNode::footprintCallback, this);
 
     collision_velocity_filter_ = new CollisionVelocityFilter();
 
@@ -29,13 +35,15 @@ CollisionVelocityFilterNode::CollisionVelocityFilterNode() :
     ROS_INFO("Reading parameter from parameter server ...");
     if (!getFootprintFromParameterServer(footprint_parameter_name))
     {
-        ROS_WARN_STREAM("Could not read parameter <" << footprint_parameter_name << "> from the parameter server. Try to receive it from the specified topic ...");
+        ROS_WARN_STREAM("Could not read parameter <" << footprint_parameter_name
+            << "> from the parameter server. Try to receive it from the specified topic ...");
         getFootprintFromTopic(footprint_topic_name);
     }
     ROS_INFO("Got footprint!");
 
     // Subscriber
-    sub_twist_ = nh.subscribe < geometry_msgs::Twist > ("cmd_vel_in", 1, &CollisionVelocityFilterNode::twistCommadCallback, this);
+    sub_twist_ = nh.subscribe < geometry_msgs::Twist > ("cmd_vel_in", 1,
+        &CollisionVelocityFilterNode::twistCommadCallback, this);
 
     // read parameters
     scan_topics = readScanTopicsFromParameterServer();
@@ -55,7 +63,16 @@ CollisionVelocityFilterNode::CollisionVelocityFilterNode() :
     double linear_velocity_in_soft_padding, angular_velocity_in_soft_padding;
     nh.param<double>("linear_velocity_in_soft_padding", linear_velocity_in_soft_padding, 0.02);
     nh.param<double>("angular_velocity_in_soft_padding", angular_velocity_in_soft_padding, 0.2);
-    collision_velocity_filter_->setVelocitiesInSoftPadding(linear_velocity_in_soft_padding, angular_velocity_in_soft_padding);
+    collision_velocity_filter_->setVelocitiesInSoftPadding(linear_velocity_in_soft_padding,
+        angular_velocity_in_soft_padding);
+
+    double angle_tolerance;
+    nh.param<double>("angle_tolerance", angle_tolerance, 2.0);
+    collision_velocity_filter_->setAngleTolerance(angle_tolerance);
+
+    double min_linear_velocity;
+    nh.param<double>("min_linear_velocity", min_linear_velocity, 0.02);
+    collision_velocity_filter_->setMinimumLinearVelocity(min_linear_velocity);
 
     if (scan_topics.size() == 0)
     {
@@ -65,7 +82,8 @@ CollisionVelocityFilterNode::CollisionVelocityFilterNode() :
     else if (scan_topics.size() == 1)
     {
         ROS_INFO_STREAM("Subscribing to one scan topic: " << scan_topics[0]);
-        sub_single_scan_ = nh.subscribe < sensor_msgs::LaserScan > (scan_topics[0], 10, &CollisionVelocityFilterNode::oneLaserScanCallback, this);
+        sub_single_scan_ = nh.subscribe < sensor_msgs::LaserScan > (scan_topics[0], 10,
+            &CollisionVelocityFilterNode::oneLaserScanCallback, this);
     }
     else
     {
@@ -78,9 +96,11 @@ CollisionVelocityFilterNode::CollisionVelocityFilterNode() :
             sub_scan_1.subscribe(nh, scan_topics[0], 10);
             sub_scan_2.subscribe(nh, scan_topics[1], 10);
 
-            two_synced_laser_scans_ = boost::make_shared<Synchronizer<ApproximateTime<sensor_msgs::LaserScan, sensor_msgs::LaserScan> > >(3);
+            two_synced_laser_scans_ = boost::make_shared<Synchronizer<ApproximateTime<sensor_msgs::LaserScan,
+                sensor_msgs::LaserScan> > >(3);
             two_synced_laser_scans_->connectInput(sub_scan_1, sub_scan_2);
-            two_synced_laser_scans_->registerCallback(boost::bind(&CollisionVelocityFilterNode::twoSynchronizedLaserscanCallback, this, _1, _2));
+            two_synced_laser_scans_->registerCallback(boost::bind(
+                &CollisionVelocityFilterNode::twoSynchronizedLaserscanCallback, this, _1, _2));
         }
         else if (scan_topics.size() == 3)
         {
@@ -88,9 +108,11 @@ CollisionVelocityFilterNode::CollisionVelocityFilterNode() :
             sub_scan_2.subscribe(nh, scan_topics[1], 10);
             sub_scan_3.subscribe(nh, scan_topics[0], 10);
 
-            three_synced_laser_scans_ = boost::make_shared<Synchronizer<ApproximateTime<sensor_msgs::LaserScan, sensor_msgs::LaserScan, sensor_msgs::LaserScan> > >(3);
+            three_synced_laser_scans_ = boost::make_shared<Synchronizer<ApproximateTime<sensor_msgs::LaserScan,
+                sensor_msgs::LaserScan, sensor_msgs::LaserScan> > >(3);
             three_synced_laser_scans_->connectInput(sub_scan_1, sub_scan_2, sub_scan_3);
-            three_synced_laser_scans_->registerCallback(boost::bind(&CollisionVelocityFilterNode::threeSynchronizedLaserscanCallback, this, _1, _2, _3));
+            three_synced_laser_scans_->registerCallback(boost::bind(
+                &CollisionVelocityFilterNode::threeSynchronizedLaserscanCallback, this, _1, _2, _3));
         }
         else
         {
@@ -109,6 +131,10 @@ CollisionVelocityFilterNode::CollisionVelocityFilterNode() :
         pub_real_footprint_ = nh.advertise < geometry_msgs::PolygonStamped > ("real_padding_footprint", 1, true);
         pub_soft_footprint_ = nh.advertise < geometry_msgs::PolygonStamped > ("soft_padding_footprint", 1, true);
         pub_hard_footprint_ = nh.advertise < geometry_msgs::PolygonStamped > ("hard_padding_footprint", 1, true);
+        pub_front_right_footprint_ = nh.advertise < geometry_msgs::PolygonStamped > ("front_right_footprint", 1, true);
+        pub_front_left_footprint_ = nh.advertise < geometry_msgs::PolygonStamped > ("front_left_footprint", 1, true);
+        pub_rear_right_footprint_ = nh.advertise < geometry_msgs::PolygonStamped > ("rear_right_footprint", 1, true);
+        pub_rear_left_footprint_ = nh.advertise < geometry_msgs::PolygonStamped > ("rear_left_footprint", 1, true);
     }
 }
 
@@ -119,6 +145,10 @@ CollisionVelocityFilterNode::~CollisionVelocityFilterNode()
         pub_hard_footprint_.shutdown();
         pub_soft_footprint_.shutdown();
         pub_real_footprint_.shutdown();
+        pub_front_right_footprint_.shutdown();
+        pub_front_left_footprint_.shutdown();
+        pub_rear_right_footprint_.shutdown();
+        pub_rear_left_footprint_.shutdown();
     }
 
     pub_event_.shutdown();
@@ -148,8 +178,7 @@ bool CollisionVelocityFilterNode::getFootprintFromParameterServer(const std::str
     if (!nh.getParam(parameter_name, footprint_param_list))
         return false;
 
-    // ToDo: read these values also as parameters
-    footprint.header.frame_id = "/base_link";
+    footprint.header.frame_id = CollisionVelocityFilterNode::target_frame_;
     footprint.header.stamp = ros::Time::now();
 
     ROS_ASSERT(footprint_param_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
@@ -194,7 +223,8 @@ void CollisionVelocityFilterNode::oneLaserScanCallback(const sensor_msgs::LaserS
 
     laser_scans_as_pcl_cloud_received = true;
 }
-void CollisionVelocityFilterNode::twoSynchronizedLaserscanCallback(const sensor_msgs::LaserScan::ConstPtr &scan_1, const sensor_msgs::LaserScan::ConstPtr &scan_2)
+void CollisionVelocityFilterNode::twoSynchronizedLaserscanCallback(const sensor_msgs::LaserScan::ConstPtr &scan_1,
+    const sensor_msgs::LaserScan::ConstPtr &scan_2)
 {
     std::vector<sensor_msgs::LaserScan> scans;
 
@@ -204,7 +234,8 @@ void CollisionVelocityFilterNode::twoSynchronizedLaserscanCallback(const sensor_
     accumulateLaserScansToPointCloud(scans);
 }
 
-void CollisionVelocityFilterNode::threeSynchronizedLaserscanCallback(const sensor_msgs::LaserScan::ConstPtr &scan_1, const sensor_msgs::LaserScan::ConstPtr &scan_2,
+void CollisionVelocityFilterNode::threeSynchronizedLaserscanCallback(const sensor_msgs::LaserScan::ConstPtr &scan_1,
+    const sensor_msgs::LaserScan::ConstPtr &scan_2,
         const sensor_msgs::LaserScan::ConstPtr &scan_3)
 {
     std::vector<sensor_msgs::LaserScan> scans;
@@ -237,12 +268,11 @@ void CollisionVelocityFilterNode::accumulateLaserScansToPointCloud(const std::ve
 sensor_msgs::PointCloud2 CollisionVelocityFilterNode::getCloudFromLaserScan(const sensor_msgs::LaserScan &scan)
 {
     sensor_msgs::PointCloud2 cloud;
-
     try
     {
-        transform_listener_.waitForTransform(scan.header.frame_id, target_frame_, scan.header.stamp, ros::Duration(0.1));
+        transform_listener_.waitForTransform(scan.header.frame_id, target_frame_, scan.header.stamp,
+            ros::Duration(0.1));
         laser_projector_.transformLaserScanToPointCloud(target_frame_, scan, cloud, transform_listener_);
-
     }
     catch (std::exception &e)
     {
@@ -268,29 +298,33 @@ void CollisionVelocityFilterNode::update()
 {
     if (desired_twist_msg_received_ && laser_scans_as_pcl_cloud_received)
     {
-        safe_twist_ = collision_velocity_filter_->calculateSafeBaseVelocities(desired_twist_msg_, laser_scans_as_pcl_cloud);
+        safe_twist_ = collision_velocity_filter_->calculateSafeBaseVelocities(desired_twist_msg_,
+            laser_scans_as_pcl_cloud);
 
         if (debug_mode_)
         {
             pub_real_footprint_.publish(collision_velocity_filter_->getRealFootprint());
             pub_soft_footprint_.publish(collision_velocity_filter_->getSoftPaddingFootprint());
             pub_hard_footprint_.publish(collision_velocity_filter_->getHardPaddingFootprint());
+            pub_front_right_footprint_.publish(collision_velocity_filter_->getFrontRightFootprint());
+            pub_front_left_footprint_.publish(collision_velocity_filter_->getFrontLeftFootprint());
+            pub_rear_right_footprint_.publish(collision_velocity_filter_->getRearRightFootprint());
+            pub_rear_left_footprint_.publish(collision_velocity_filter_->getRearLeftFootprint());
         }
 
         // publish safe velocities
         pub_safe_twist_.publish(safe_twist_);
 
         // publish events
-        double sum_desired = fabs(desired_twist_msg_.linear.x) + fabs(desired_twist_msg_.linear.y) + fabs(desired_twist_msg_.angular.x);
+        double sum_desired = fabs(desired_twist_msg_.linear.x) + fabs(desired_twist_msg_.linear.y)
+            + fabs(desired_twist_msg_.angular.x);
         double sum_safe = fabs(safe_twist_.linear.x) + fabs(safe_twist_.linear.y) + fabs(safe_twist_.angular.x);
 
-        if (sum_desired > 0.0 and sum_safe == 0.0)
-        {
+        if (sum_desired > 0.0 && sum_safe == 0.0) {
             event_out_.data = "e_zero_velocities_forwarded";
             pub_event_.publish(event_out_);
         }
-        else if (sum_desired != sum_safe)
-        {
+        else if (sum_desired != sum_safe) {
             event_out_.data = "e_reduced_velocities_forwarded";
             pub_event_.publish(event_out_);
         }
@@ -318,5 +352,4 @@ int main(int argc, char **argv)
 
         loop_rate.sleep();
     }
-
 }
