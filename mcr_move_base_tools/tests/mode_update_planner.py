88af23d9909321754e3313c93219c7c133d68022
Jose Mayoral <jocamaba1989@gmail.com>
2018-09-04 13:19:52 +0200

    error in mode_update_planner found and corrected

--- mcr_move_base_tools/tests/mode_update_planner.py
+++ mcr_move_base_tools/tests/mode_update_planner.py
@@ -4,4 +4,3 @@ from mcr_move_base_tools.tools import PlannerUpdater
 
 desired_mode = raw_input('Type desired_mode: ')
 planner_updater = PlannerUpdater(mode_request=desired_mode)
-planner_updater.update_planners(new_config, new_global_planner_ns=global_planner_ns, new_local_planner_ns=local_planner_ns)
